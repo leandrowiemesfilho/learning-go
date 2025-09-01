1e88a23f1ff32d1594d0520d3e3f59cda8df36ed
Leandro Wiemes Filho <leandro.wiemesfilho@gmail.com>
2025-09-01 19:55:46 +0100

    Applicational exercises including sorting; marshall; unmarshall and encoders

---
+++ 8-applications/exercise-1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+/*
+Use marshal to transform []user into JSON
+*/
+
+type user struct {
+	First string `json:"first_name"`
+	Age   int    `json:"age"`
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	// your code goes here
+	marshal, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(marshal))
+}
